507c9b0525f0e0cf47e20ab941ae9948bcecee5d
Rama Bena <ramabena.rb@gmail.com>
2022-05-02 05:54:10 +0800

    Membuat data class color

---
+++ color.py
@@ -0,0 +1,6 @@
+class Color:
+    SCORE = (255, 255, 255) # white
+    FOOD  = (200, 0, 0) # red
+    BACKGROUND = (0, 0, 0) # black
+    SNAKE_BODY = (0, 0, 255) # blue
+    SNAKE_BORDER = (0, 100, 255) # dark blue
