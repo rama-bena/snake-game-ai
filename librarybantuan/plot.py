a7830e65e4b34f49597568e499b73cf0a5f7a05c
rama_pc_nara <ramabena.rb@gmail.com>
2022-05-20 17:24:42 +0800

    selesai vr1 eps no

--- librarybantuan/plot.py
+++ librarybantuan/plot.py
@@ -1,26 +1,38 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import interpolate
 from IPython import display
 from captum.attr import IntegratedGradients
 
+degrees = [2, 5, 10, 20, 50]
 
-plt.ion()
+def make_smooth_line(scores, degree):
+    setengah = int((len(scores)/degree)//2 + 1)
+    rata_rata = []
+    for i in range(0, len(scores)):
+        dari = max(i-setengah, 0)
+        sampai = min(i+setengah+1, len(scores))
+        data = scores[dari:sampai]
+        rata_rata.append(sum(data)/len(data))
+    return rata_rata
 
 def plot(scores, title, interative=True):
-    # display.clear_output(wait=True)
-    # display.display(plt.gcf())
+    plt.ion()
     plt.clf()
     plt.title(title)
     plt.xlabel('Number of Games')
-    plt.ylabel('Value')
+    plt.ylabel('Score')
 
     plt.plot(scores, label='score')
-    # plt.plot(rewards, label='reward')
-
     plt.text(len(scores)-1, scores[-1], str(scores[-1]))
-    # plt.text(len(rewards)-1, rewards[-1], str(rewards[-1]))
     
+    # if len(scores) > 10*degrees[0]:
+    #     degrees.pop(0)
+    # rata_rata = make_smooth_line(scores, max(len(scores)//5, 2))
+    # plt.plot(rata_rata, label='rata-rata')
+    # plt.text(len(rata_rata)-1, rata_rata[-1], str(rata_rata[-1]))
+
     plt.legend()
 
     if interative:
@@ -29,23 +41,15 @@ def plot(scores, title, interative=True):
     else:
         plt.show(block=True)
 
-def feature_importance(model, state, target, title):
+def feature_importance(visual_range, model, state, x_pos, target, label):
         ig = IntegratedGradients(model)
         state = torch.tensor(state, dtype=torch.float)        
         state.requires_grad_()
-        feature_names = list(np.arange(81)) + ['up', 'right', 'down', 'left', 'iteration']
-        # target_names = ['Straight', 'Turn Right', 'Turn Left']
-        x_pos = (np.arange(len(feature_names)))
-    
-        plt.figure(figsize=(25, 10))
-        plt.title(title)
-        plt.xlabel("Features")
-        plt.xticks(x_pos, feature_names, wrap=True, rotation=90)        
+             
         # for target in range(3):
         attr, delta = ig.attribute(state, target=target, return_convergence_delta=True)
         attr = attr.detach().numpy()
         importances = np.mean(attr, axis=0)
-        plt.bar(x_pos, importances, align='center')
+        plt.bar(x_pos, importances, align='center', label=label)
         
         plt.grid()
-        plt.show()
--- main.py
+++ main.py
@@ -2,33 +2,42 @@ from game import SnakeGameAI
 from agent import Agent
 from librarybantuan.plot import plot
 import pickle
+import time
+
+def format_time(seconds):
+    second = seconds % 60
+    minute = seconds // 60
+    hour = minute // 60
+    minute = minute % 60
+
+    return f"{hour}h {minute}m {round(second,2)}s"
+
 
 if __name__ == '__main__':
     #* Variabel
     scores     = [0] # game ke-0 nilainya 0
-    best_score = 0
+    best_score = (0, 0) # nilai best score, game menghasilkan best score
     total_reward = 0
-    # rewards = [0]
-    title = "vr=9, eps=no_eps"
 
     #* Argumen
-    width        = 640
-    height       = 480
     speed        = 0      # semakin tinggi semakin cepet, khusus 0 paling cepet
     epsilon_rate = 100    # pengurangan gerakan random, lebih atau sama dengan 100 -> tanpa random
     max_memory   = 100_000
+    visual_range = 1
+    title = "vr=1, eps=no_eps"
 
     #* Buat object game dan agent
-    agent = Agent(max_memory=max_memory, epsilon_rate=epsilon_rate)
-    game = SnakeGameAI(width=width, height=height, speed=speed)
+    agent = Agent(visual_range=visual_range, max_memory=max_memory, epsilon_rate=epsilon_rate)
+    game = SnakeGameAI(speed=speed)
     try:
+        start = time.time()
         while True:
             # dapatkan state sekarang
             state_old = agent.get_state(game)
             # cari gerakan sesuai dengan state sekarang
             action = agent.get_action(state_old)
             with_ui = agent.n_games > 100
-            # with_ui = True
+            with_ui = True
             try:
                 # lakukan gerakannya
                 reward, game_over, caution_death, score = game.play_step(action, with_ui)
@@ -45,17 +54,17 @@ if __name__ == '__main__':
             agent.train_short_memory(state_old, action, reward, state_new, game_over)
 
             if game_over:
-                # rewards.append(total_reward)
                 scores.append(score)
                 plot(scores, title=title)
                 game.reset()
                 agent.n_games += 1
-                if score > best_score:
-                    best_score = score
+                if score > best_score[0]:
+                    best_score = (score, agent.n_games)
                     agent.model.save()
                 print(f"Game: {agent.n_games}")
-                print(f"Tingkat random: {max(100 - agent.epsilon_rate*agent.n_games, 0)}%")
-                print(f"Score: {score}, Best score: {best_score}")
+                print(f"Time: {format_time(time.time()-start)}")
+                print(f"Randomness: {max(100 - agent.epsilon_rate*agent.n_games, 0)}%")
+                print(f"Score: {score}, Best score: {best_score[0]}, {best_score[1]}")
                 print(f"Reward: {total_reward}")
                 print(f"Caution Death:{caution_death}")
                 print()
@@ -67,7 +76,7 @@ if __name__ == '__main__':
         print('keluar paksa')
     finally:
         print('finally')
-        plot(scores, title=title, interative=False)
         with open('./model/memory.mem', "wb") as f:
             pickle.dump(agent.memory, f)
+        plot(scores, title=title, interative=False)
             
